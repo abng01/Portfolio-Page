f04df408b485ae2dd590552ef0114c1bc314ef3f
abng01 <alicia.bang404@gmail.com>
2025-10-22 22:33:51 +1100

    added content to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material'
 
 export default function HomePage() {
   return (
-    <div>
-      Hello
-    </div>
+    <Box>
+      <Box>
+        <Typography variant="h5">Hi there! I'm </Typography>
+        <Typography variant="h1" sx={{fontWeight: "bold", color: "#84badcff"}}>Alicia Bang</Typography>
+        <Typography variant="h3">Full-stack developer</Typography>
+        <Typography variant="p">I'm a passionate developer focused on creating modern, intuitive and impactful web applications.</Typography>
+      </Box>
+    </Box>
   )
 }
