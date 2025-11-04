019498b527f362511ac56fbc3f969aab1c2a359a
abng01 <alicia.bang404@gmail.com>
2025-11-04 19:37:34 +1100

    added placeholder image and aligned text in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Box, Typography } from '@mui/material'
+import placeholderImg from '../assets/Placeholder_view_vector.svg.png'
 
 export default function HomePage() {
   return (
-    <Box>
-      <Box>
-        <Typography variant="h5">Hi there! I'm </Typography>
+    <Box sx={{display: "flex", flexWrap: "wrap", gap: 5}}>
+      <Box sx={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+        <Typography variant="h5" sx={{marginBottom: 2}}>Hi there! I'm </Typography>
         <Typography variant="h1" sx={{fontWeight: "bold", color: "#84badcff"}}>Alicia Bang</Typography>
         <Typography variant="h3">Full-stack developer</Typography>
-        <Typography variant="p">I'm a passionate developer focused on creating modern, intuitive and impactful web applications.</Typography>
+        <Typography sx={{marginTop: 3}}>I'm a passionate developer focused on creating modern, intuitive and impactful web applications.</Typography>
       </Box>
+      <img src={placeholderImg} />
     </Box>
   )
 }
